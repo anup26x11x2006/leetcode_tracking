e666986a9a8fad27d45dd51cf071e41fb5aa9f6d
Anup <anooppatidar07@gmail.com>
2026-07-01 19:54:31 +0530

    Time: 64 ms (99.68%), Space: 123.3 MB (31.75%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -1,56 +1,84 @@
 class Solution {
-    final int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     public int maximumSafenessFactor(List<List<Integer>> grid) {
         int n = grid.size();
-        int[][] mat = new int[n][n];
-        Queue<int[]> multiSourceQueue = new LinkedList<>();
+        if (grid.get(0).get(0) == 1 || grid.get(n - 1).get(n - 1) == 1 || n == 1)
+            return 0;
+
+        int arr[][] = new int[n][n];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid.get(i).get(j) == 1) {
-                    multiSourceQueue.add(new int[] {i, j});
-                    mat[i][j] = 0;
-                } else {
-                    mat[i][j] = -1;
-                }
+                arr[i][j] = grid.get(i).get(j);
             }
         }
-        while (!multiSourceQueue.isEmpty()) {
-            int size = multiSourceQueue.size();
-            while (size-- > 0) {
-                int[] curr = multiSourceQueue.poll();
-                for (int[] d : dir) {
-                    int di = curr[0] + d[0];
-                    int dj = curr[1] + d[1];
-                    int val = mat[curr[0]][curr[1]];
-                    if (isValidCell(mat, di, dj) && mat[di][dj] == -1) {
-                        mat[di][dj] = val + 1;
-                        multiSourceQueue.add(new int[] {di, dj});
-                    }
-                }
-            }
-        }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[2] - a[2]);
-        pq.add(new int[] {0, 0, mat[0][0]});
-        mat[0][0] = -1;
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
+        arr = updateMatrix(arr);
 
-            if (curr[0] == n - 1 && curr[1] == n - 1) {
-                return curr[2];
-            }
-            for (int[] d : dir) {
-                int di = d[0] + curr[0];
-                int dj = d[1] + curr[1];
-                if (isValidCell(mat, di, dj) && mat[di][dj] != -1) {
-                    pq.add(new int[] {di, dj, Math.min(curr[2], mat[di][dj])});
-                    mat[di][dj] = -1;
+        int low = 0, high = (n - 1) * 2;
+        int mid = 0;
+        int result = mid;
+        while (low <= high) {
+            mid = (high - low) / 2 + low;
+            if (dfs(arr, 0, 0, mid, n, new boolean[n][n])) {
+                result = mid;
+                low = mid + 1;
+            } else
+                high = mid - 1;
+        }
+        return result;
+    }
+
+    public int[][] updateMatrix(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                matrix[row][col] ^= 1;
+                if (matrix[row][col] == 1) {
+                    matrix[row][col] = Integer.MAX_VALUE / 2;
                 }
             }
         }
-        return -1;
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 1; col < n; col++) {
+                matrix[row][col] = Math.min(matrix[row][col], matrix[row][col - 1] + 1);
+            }
+            for (int col = n - 2; col >= 0; col--) {
+                matrix[row][col] = Math.min(matrix[row][col], matrix[row][col + 1] + 1);
+            }
+        }
+
+        for (int col = 0; col < n; col++) {
+            for (int row = 1; row < n; row++) {
+                matrix[row][col] = Math.min(matrix[row][col], matrix[row - 1][col] + 1);
+            }
+            for (int row = n - 2; row >= 0; row--) {
+                matrix[row][col] = Math.min(matrix[row][col], matrix[row + 1][col] + 1);
+            }
+        }
+
+        return matrix;
     }
-    private boolean isValidCell(int[][] mat, int i, int j) {
-        int n = mat.length;
-        return i >= 0 && j >= 0 && i < n && j < n;
+
+    int[] dx = { -1, 1, 0, 0 };
+    int[] dy = { 0, 0, 1, -1 };
+
+    boolean dfs(int arr[][], int r, int c, int mid, int n, boolean[][] visited) {
+        if (visited[r][c] || arr[r][c] < mid)
+            return false;
+        if (r == n - 1 && c == n - 1)
+            return true;
+
+        visited[r][c] = true;
+        for (int i = 0; i < 4; i++) {
+            int x = r + dx[i];
+            int y = c + dy[i];
+
+            if (x == -1 || y == -1 || x == n || y == n)
+                continue;
+            if (dfs(arr, x, y, mid, n, visited))
+                return true;
+        }
+        return false;
     }
 }
