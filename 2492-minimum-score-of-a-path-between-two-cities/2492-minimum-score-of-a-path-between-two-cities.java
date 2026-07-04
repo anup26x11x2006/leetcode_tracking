e6d82d112cab6c0912afeb7ff4c309c11e72deaf
Anup <anooppatidar07@gmail.com>
2026-07-04 17:04:31 +0530

    Time: 6 ms (100%), Space: 127.7 MB (98.25%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -0,0 +1,40 @@
+class Solution {
+    private int[] parent;
+    private int[] minEdge;
+
+    public int minScore(int n, int[][] roads) {
+        parent = new int[n+1];
+        minEdge = new int[n+1];
+
+        for (int i=0; i<=n; ++i) {
+            parent[i] = i;
+            minEdge[i] = Integer.MAX_VALUE;
+        }
+
+        for (int[] road : roads) {
+            union(road[0], road[1], road[2]);
+        }
+
+        return minEdge[find(1)];
+    }
+
+    private int find(int x) {
+        // path compression
+        while (parent[x] != x) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+
+    private void union(int a, int b, int d) {
+        int parentA = find(a);
+        int parentB = find(b);
+        if (parentA != parentB) {
+            parent[parentA] = parentB;
+            minEdge[parentB] = Math.min(minEdge[parentB], Math.min(minEdge[parentA], d));
+        } else {
+            minEdge[parentA] = Math.min(minEdge[parentA], d);
+        }
+    }
+}
