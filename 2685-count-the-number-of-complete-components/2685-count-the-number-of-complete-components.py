19bd9348ce05a0e0c9725fb27738d36566c45c79
Anup <anooppatidar07@gmail.com>
2026-07-11 21:07:25 +0530

    Time: 68 ms (52.7%), Space: 12.7 MB (68.92%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -1,32 +1,38 @@
-class Solution:
+class Solution(object):
     def countCompleteComponents(self, n, edges):
-
-        graph = defaultdict(list)
-
-        for v1, v2 in edges:
-            graph[v1].append(v2)
-            graph[v2].append(v1)
-
-        complete_count = 0
-        visited = set()
-
-        def _dfs(curr, info):
-            visited.add(curr)
-            info[0] += 1
-            info[1] += len(graph[curr])
-
-            for next_vertex in graph[curr]:
-                if next_vertex not in visited:
-                    _dfs(next_vertex, info)
-
-        for vertex in range(n):
-            if vertex in visited:
-                continue
-
-            component_info = [0, 0]
-            _dfs(vertex, component_info)
-
-            if component_info[0] * (component_info[0] - 1) == component_info[1]:
-                complete_count += 1
-
-        return complete_count
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        adj = [[] for _ in range(n)]
+        cnt = 0
+        visited = [0]*n
+        
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        for i in range(n):
+            if visited[i] == 0:
+                comp_nodes = []
+                stack = [i]
+                visited[i] = 1
+                
+                while stack:
+                    node = stack.pop()
+                    comp_nodes.append(node)
+                    for j in adj[node]:
+                        if visited[j] == 0:
+                            stack.append(j)
+                            visited[j] = 1
+                v_count = len(comp_nodes)
+                degree_sum = 0
+                for node in comp_nodes:
+                    degree_sum+=len(adj[node])
+                
+                actual_edges = degree_sum//2
+                expected_edges = (v_count)*(v_count-1)//2
+                if actual_edges == expected_edges:
+                    cnt+=1
+        return cnt
