b051c76c462c5f7a75f878ce1d23fc60cb78a369
Anup <anooppatidar07@gmail.com>
2026-07-24 21:05:59 +0530

    Time: 47 ms (99.44%), Space: 48.2 MB (66.29%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,36 +1,67 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        int m = 0;
-        for (int v : nums) {
-            m = max(m, v);
+        const auto& bit_length = [](int64_t x) {
+            return (x ? std::__lg(x) : -1) + 1;
+        };
+
+        vector<int64_t> a(1 << bit_length(ranges::max(nums)));
+        for (const auto& x: nums) {
+            ++a[x];
         }
-        int u = 1;
-        while (u <= m) {
-            u <<= 1;
+        FST(a, false);
+        for (int i = 0; i < size(a); i++) {
+           a[i] = a[i] * a[i] * a[i];
         }
-        vector<int> s(u);
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                s[nums[i] ^ nums[j]] = 1;
+        FST(a, true);
+        int result = 0;
+        for (const auto& x: a) {
+            if (x) {
+                ++result;
             }
         }
-        vector<int> t(u);
-        for (int x = 0; x < u; x++) {
-            if (!s[x]) {
-                continue;
-            }
-            for (int v : nums) {
-                t[x ^ v] = 1;
-            }
-        }
-        int ans = 0;
-        for (int x = 0; x < u; x++) {
-            if (t[x]) {
-                ans++;
+        return result;
+    }
+
+private:
+    void FST(auto& a, bool inv) {
+        for (int n = size(a), step = 1; step < n; step <<= 1) {
+            for (int i = 0; i < n; i += step << 1) {
+                for (int j = i; j < i + step; ++j) {
+                    auto &u = a[j], &v = a[j + step];
+                    tie(u, v) = pair(u + v, u - v);
+                }
             }
         }
-        return ans;
+        if (inv) {
+            for (auto& x : a) {
+                x /= size(a);
+            }
+        }
+    }
+};
+
+class Solution2 {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        const auto& bit_length = [](int64_t x) {
+            return (x ? std::__lg(x) : -1) + 1;
+        };
+
+        unordered_set<int> cnt2 = {0};
+        unordered_set<int> cnt3;
+        const int max_cnt = 1 << bit_length(ranges::max(nums));
+        for (const auto& x : nums) {
+            for (const auto& y : cnt2) {
+                cnt3.emplace(x ^ y);
+            }
+            for (const auto& y : nums) {
+                cnt2.emplace(x ^ y);
+            }
+            if (size(cnt3) == max_cnt) {
+                break;
+            }
+        }
+        return size(cnt3);
     }
 };
